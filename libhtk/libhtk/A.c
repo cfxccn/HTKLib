c4c1ce0948fb4325795af8becdd1ddacf60f88e5
cfxccn <ccnx@qq.com>
2014-12-12 22:47:51 +0800

    init C code Signed-off-by: cfxccn <ccnx@qq.com>

---
+++ libhtk/libhtk/A.c
@@ -0,0 +1,16 @@
+
+
+#include "tool.h"
+
+
+int main(int argc, char *argv[]){
+main_HCopy(argc, argv);
+	main_HResult(argc, argv);
+	main_HRest(argc, argv);
+ main_HParse(argc, argv);
+
+	main_HSLab(argc, argv);
+
+main_HVite(argc, argv);
+}
+
