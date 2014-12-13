5735a5740baf0e3c683e93825928d3b9c9d9612b
cfxccn <ccnx@qq.com>
2014-12-13 11:04:13 +0800

    Hinit remove exit Signed-off-by: cfxccn <ccnx@qq.com>

--- libhtk/libhtk/A.c
+++ libhtk/libhtk/A.c
@@ -1,16 +1,15 @@
-
-
 #include "tool.h"
 
 
 int main(int argc, char *argv[]){
-main_HCopy(argc, argv);
-	main_HResult(argc, argv);
-	main_HRest(argc, argv);
- main_HParse(argc, argv);
+	//main_HCopy(argc, argv);
+	main_HInit(argc, argv);
 
-	main_HSLab(argc, argv);
 
-main_HVite(argc, argv);
+	//main_HResult(argc, argv);
+	//main_HRest(argc, argv);
+	//main_HParse(argc, argv);
+	//main_HSLab(argc, argv);
+	//main_HVite(argc, argv);
 }
 
